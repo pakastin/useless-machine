f07d786625b1af8b7356e12d4f5f6c5fbc7e3c7e
Juha Lindstedt <juha@pakastin.fi>
2021-11-16 21:06:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ const go = async () => {
 
 document.addEventListener('keydown', go);
 
-$toggle.onclick = go;
+$toggle.onclick = $toggle.onmousedown = go;
 
 const wait = async (time) => new Promise((resolve) => setTimeout(resolve, time));
 
